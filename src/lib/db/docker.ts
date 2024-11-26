fe5886256d7320c64b2fe5adf4d24f18f5885e8a
Shivam Deepak Chaudhary <shivchaudhary790@hotmail.com>
2024-11-26 23:45:51 +0530

    fixed user relation issue 2

--- src/lib/db/docker.ts
+++ src/lib/db/docker.ts
@@ -33,7 +33,7 @@ export async function getContainerById(id: string) {
 }
 
 export async function createContainer(container: DatabaseInstance) {
-  const { name, port, envVars, type, image, ...rest } = container;
+  const { name, port, envVars, type, image, userId, ...rest } = container;
   
   return await prisma.databaseInstance.create({ 
     data: {
@@ -42,6 +42,7 @@ export async function createContainer(container: DatabaseInstance) {
       type,
       image,
       envVars: envVars as Prisma.InputJsonValue,
+      userId,
       ...rest
     } 
   });
